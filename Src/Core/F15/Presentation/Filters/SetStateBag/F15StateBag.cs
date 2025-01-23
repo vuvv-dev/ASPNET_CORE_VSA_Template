656765130e9a7a8fecd1bbdc148c1e1fd8a64f28
Khoa Le <ledinhdangkhoa10a9@gmail.com>
2025-01-23 14:34:17 +0700

    fix(F15): Add some changes related to state bag and mapper

---
+++ Src/Core/F15/Presentation/Filters/SetStateBag/F15StateBag.cs
@@ -0,0 +1,8 @@
+namespace F15.Presentation.Filters.SetStateBag;
+
+public sealed class F15StateBag
+{
+    public F15Request HttpRequest { get; set; }
+
+    public F15Response HttpResponse { get; set; }
+}
