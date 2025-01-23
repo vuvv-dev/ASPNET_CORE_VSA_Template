c90e41088279289bc78bf8eb1dce33dcff456432
Khoa Le <ledinhdangkhoa10a9@gmail.com>
2025-01-23 11:12:32 +0700

    fix(F5): Add some changes related to state bag and mapper

--- Src/Core/F5/Mapper/F5HttpResponseMapper.cs
+++ Src/Core/F5/Mapper/F5HttpResponseMapper.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using F5.Common;
 using F5.Models;
 using F5.Presentation;
+using F5.Presentation.Filters.SetStateBag;
 using Microsoft.AspNetCore.Http;
 
 namespace F5.Mapper;
@@ -11,7 +12,7 @@ public static class F5HttpResponseMapper
 {
     private static ConcurrentDictionary<
         F5Constant.AppCode,
-        Func<F5AppRequestModel, F5AppResponseModel, F5Response>
+        Func<F5AppRequestModel, F5AppResponseModel, HttpContext, F5Response>
     > _httpResponseMapper;
 
     private static void Init()
@@ -23,34 +24,55 @@ public static class F5HttpResponseMapper
 
         _httpResponseMapper.TryAdd(
             F5Constant.AppCode.PASSWORD_IS_INVALID,
-            (appRequest, appResponse) => F5Constant.DefaultResponse.Http.PASSWORD_IS_INVALID
+            (appRequest, appResponse, httpContext) =>
+            {
+                return F5Constant.DefaultResponse.Http.PASSWORD_IS_INVALID;
+            }
         );
 
         _httpResponseMapper.TryAdd(
             F5Constant.AppCode.SERVER_ERROR,
-            (appRequest, appResponse) => F5Constant.DefaultResponse.Http.SERVER_ERROR
+            (appRequest, appResponse, httpContext) =>
+            {
+                return F5Constant.DefaultResponse.Http.SERVER_ERROR;
+            }
         );
 
         _httpResponseMapper.TryAdd(
             F5Constant.AppCode.SUCCESS,
-            (appRequest, appResponse) =>
-                new()
+            (appRequest, appResponse, httpContext) =>
+            {
+                return new()
                 {
                     HttpCode = StatusCodes.Status200OK,
                     AppCode = (int)F5Constant.AppCode.SUCCESS,
-                }
+                };
+            }
         );
 
         _httpResponseMapper.TryAdd(
             F5Constant.AppCode.TOKEN_DOES_NOT_EXIST,
-            (appRequest, appResponse) => F5Constant.DefaultResponse.Http.TOKEN_DOES_NOT_EXIST
+            (appRequest, appResponse, httpContext) =>
+            {
+                return F5Constant.DefaultResponse.Http.TOKEN_DOES_NOT_EXIST;
+            }
         );
     }
 
-    public static F5Response Get(F5AppRequestModel appRequest, F5AppResponseModel appResponse)
+    public static F5Response Get(
+        F5AppRequestModel appRequest,
+        F5AppResponseModel appResponse,
+        HttpContext httpContext
+    )
     {
         Init();
 
-        return _httpResponseMapper[appResponse.AppCode](appRequest, appResponse);
+        var stateBag = httpContext.Items[nameof(F5StateBag)] as F5StateBag;
+
+        var httpResponse = _httpResponseMapper[appResponse.AppCode]
+            (appRequest, appResponse, httpContext);
+        stateBag.HttpResponse = httpResponse;
+
+        return httpResponse;
     }
 }
--- Src/Core/F5/Presentation/F5StateBag.cs
+++
@@ -1,8 +0,0 @@
-namespace F5.Presentation;
-
-public sealed class F5StateBag
-{
-    public long ResetPasswordTokenId { get; set; }
-
-    public long UserId { get; set; }
-}
