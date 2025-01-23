4d5663ade06f67c066c7870633ac30b497e40232
Khoa Le <ledinhdangkhoa10a9@gmail.com>
2025-01-23 14:03:27 +0700

    fix(F9): Add some changes related to state bag and mapper

--- Src/Core/F9/Mapper/F9HttpResponseMapper.cs
+++ Src/Core/F9/Mapper/F9HttpResponseMapper.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using F9.Common;
 using F9.Models;
 using F9.Presentation;
+using F9.Presentation.Filters.SetStateBag;
 using Microsoft.AspNetCore.Http;
 
 namespace F9.Mapper;
@@ -11,7 +12,7 @@ public static class F9HttpResponseMapper
 {
     private static ConcurrentDictionary<
         F9Constant.AppCode,
-        Func<F9AppRequestModel, F9AppResponseModel, F9Response>
+        Func<F9AppRequestModel, F9AppResponseModel, HttpContext, F9Response>
     > _httpResponseMapper;
 
     private static void Init()
@@ -23,29 +24,47 @@ public static class F9HttpResponseMapper
 
         _httpResponseMapper.TryAdd(
             F9Constant.AppCode.SERVER_ERROR,
-            (appRequest, appResponse) => F9Constant.DefaultResponse.Http.SERVER_ERROR
+            (appRequest, appResponse, httpContext) =>
+            {
+                return F9Constant.DefaultResponse.Http.SERVER_ERROR;
+            }
         );
 
         _httpResponseMapper.TryAdd(
             F9Constant.AppCode.SUCCESS,
-            (appRequest, appResponse) =>
-                new()
+            (appRequest, appResponse, httpContext) =>
+            {
+                return new()
                 {
                     AppCode = (int)F9Constant.AppCode.SUCCESS,
                     HttpCode = StatusCodes.Status200OK,
-                }
+                };
+            }
         );
 
         _httpResponseMapper.TryAdd(
             F9Constant.AppCode.LIST_DOES_NOT_EXIST,
-            (appRequest, appResponse) => F9Constant.DefaultResponse.Http.LIST_DOES_NOT_EXIST
+            (appRequest, appResponse, httpContext) =>
+            {
+                return F9Constant.DefaultResponse.Http.LIST_DOES_NOT_EXIST;
+            }
         );
     }
 
-    public static F9Response Get(F9AppRequestModel appRequest, F9AppResponseModel appResponse)
+    public static F9Response Get(
+        F9AppRequestModel appRequest,
+        F9AppResponseModel appResponse,
+        HttpContext httpContext
+    )
     {
         Init();
 
-        return _httpResponseMapper[appResponse.AppCode](appRequest, appResponse);
+        var stateBag = httpContext.Items[nameof(F9StateBag)] as F9StateBag;
+
+        var httpResponse = _httpResponseMapper[appResponse.AppCode]
+            (appRequest, appResponse, httpContext);
+        stateBag.HttpResponse = httpResponse;
+
+        return httpResponse;
     }
 }
---
+++ Src/Core/F9/Presentation/Filters/SetStateBag/F9StateBag.cs
@@ -0,0 +1,8 @@
+namespace F9.Presentation.Filters.SetStateBag;
+
+public sealed class F9StateBag
+{
+    public F9Request HttpRequest { get; set; }
+
+    public F9Response HttpResponse { get; set; }
+}
