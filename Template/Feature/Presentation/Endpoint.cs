997b808d904f56814c296fe67ac55726456ba880
Khoa Le <ledinhdangkhoa10a9@gmail.com>
2025-02-09 15:45:52 +0700

    More fix

--- Template/Feature/Presentation/Endpoint.cs
+++ Template/Feature/Presentation/Endpoint.cs
@@ -24,19 +24,15 @@ public sealed class Endpoint : ControllerBase
     }
 
     /// <summary>
-    ///     Endpoint for register new user.
+    ///     Endpoint description.
     /// </summary>
     /// <param name="request">
     ///     Incoming request.
     /// </param>
-    /// <response code="422">PASSWORD_IS_INVALID</response>
-    /// <response code="409">EMAIL_ALREADY_EXISTS</response>
     /// <response code="400">VALIDATION_FAILED</response>
     /// <response code="500">SERVER_ERROR</response>
     /// <response code="200">SUCCESS</response>
     /// <response code="1">EXAMPLE RESPONSE OF ALL STATUS CODES</response>
-    [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
-    [ProducesResponseType(StatusCodes.Status409Conflict)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     [ProducesResponseType(StatusCodes.Status200OK)]
@@ -48,7 +44,7 @@ public sealed class Endpoint : ControllerBase
     [ServiceFilter<SetStateBagFilter>]
     [ServiceFilter<ValidationFilter>]
     public async Task<IActionResult> ExecuteFeatTemplateAsync(
-        [FromBody] [Required] Request request,
+        [FromBody][Required] Request request,
         CancellationToken ct
     )
     {
